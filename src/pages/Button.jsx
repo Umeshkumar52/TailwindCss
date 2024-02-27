9f84bbd3f0b7864ef6d99ce7a6b87d3db9321558
Umeshkumar52 <123877527+Umeshkumar52@users.noreply.github.com>
2024-02-27 15:22:46 +0530

    Delete src/pages directory

--- src/pages/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Button({text,onClick}) {
-  return (
-    <div>
-      <button className='bg-indigo-600 px-4 py-2 font-semibold' onClick={onClick}>{text}</button>
-    </div>
-  )
-}
